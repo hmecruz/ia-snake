42ae74db1065e4a72cb9a4b1ef5410b32c6d1d98
Diogo Borges <diogomnborges@ua.pt>
2024-11-10 22:04:26 +0000

    fix run.sh

--- run.sh
+++ run.sh
@@ -16,22 +16,3 @@ sleep 0.1
 
 # Abra o terceiro terminal em modo "fullscreen"
 xterm -geometry 1800x1012 -hold -e "cd $DIR; $ACTIVATE_CMD; python3 student.py" &
-
-#!/bin/bash
-
-# Caminho do diretório atual
-DIR="$(cd "$(dirname "$0")" && pwd)"
-
-# Comando para ativar o ambiente virtual
-ACTIVATE_CMD="source venv/bin/activate"
-
-# Abra o primeiro terminal em modo "fullscreen"
-xterm -geometry 1800x1012 -hold -e "cd $DIR; $ACTIVATE_CMD; python3 server.py" &
-sleep 0.1
-
-# Abra o segundo terminal em modo "fullscreen"
-xterm -geometry 1800x1012 -hold -e "cd $DIR; $ACTIVATE_CMD; python3 viewer.py" &
-sleep 0.1
-
-# Abra o terceiro terminal em modo "fullscreen"
-xterm -geometry 1800x1012 -hold -e "cd $DIR; $ACTIVATE_CMD; python3 student.py" &
